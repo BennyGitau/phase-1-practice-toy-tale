b7dcf953f42ff84e26867501054059ee8e088602
BensonGitau <bensonkamaugitau@gmail.com>
2024-04-04 23:23:32 +0300

    Complete lab

--- src/index.js
+++ src/index.js
@@ -12,4 +12,77 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  //fetching all toys and rendering to on the DOM
+  const toyUrl = "http://localhost:3000/toys";
+    fetch(toyUrl)
+    .then((response)=> response.json())
+    .then((toyData) => toyData.forEach((toy) =>{handleToy(toy)}))
+    .catch((error) => console.log(error))
+
+  //function to handle toys
+  function handleToy(toy) {
+    const card = document.createElement('div')
+      card.className = 'card'
+      card.innerHTML =`
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class = "toy-avatar">
+      <p>${toy.likes} likes</p>
+      <button class="like-btn" id="${toy.id}">Like ❤️</button>
+      `
+  document.getElementById('toy-collection').appendChild(card);
+  card.querySelector(".like-btn").addEventListener('click', ()=> {
+    toy.likes++;
+    card.querySelector("p").textContent = toy.likes;
+    updateToy(toy)
+  })
+
+  }
+///adding submit function
+  document.querySelector(".add-toy-form").addEventListener('submit', handleSubmit)
+
+  //handle submit function
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    let newToy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+  
+    handleToy(newToy)
+    addNewToy(newToy)
+
+  }
+  ///adding a new toy
+  function addNewToy(newToy) {
+      fetch(toyUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify(newToy),
+
+      })
+      .then(res => res.json())
+      .then(toy => console.log(toy));
+
+    }
+
+  ///making a patch request
+  function updateToy (toy) {
+    fetch(`http://localhost:3000/toys/${toy.id}`,{
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(toy)
+    })
+    .then(response => response.json())
+    .then(toy => console.log(toy))
+  }
+
+
 });
